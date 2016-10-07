0917d06b2fec256af945f5f6fbddcb8546fcd327
Patricia Holliman <trish@Emmy.hsd1.ma.comcast.net>
2016-10-07 17:27:32 -0400

    Add support for set of observable to null if pikaday closes with no choice.

--- ko-pikaday.js
+++ ko-pikaday.js
@@ -1,11 +1,9 @@
 // ------------------ ko Custom pikaday Binding -------------------------
 
-    
     ko.bindingHandlers.pikaday = {
-        init: function(element, valueAccessor, allBindings){
-            
+        init: function(element, valueAccessor, allBindings){     
             var minDate = ko.unwrap(allBindings.get('minDate')) || null,
-                maxDate = ko.unwrap(allBindings.get('maxDate')) || null
+                maxDate = ko.unwrap(allBindings.get('maxDate')) || null,
                 disableWeekends = ko.unwrap(allBindings.get('disableWeekends')) || null;
             
             // use valueAccessor().pikstore to store the pikaday object on the observable
@@ -14,18 +12,25 @@
             //       the object would still be available to the update callback of the binding
             //       but not in the viewmodel
             valueAccessor().pikstore = new Pikaday({ field: element,
-                                                 minDate: minDate,
-                                                 maxDate: maxDate,
-                                                 disableWeekends: disableWeekends,
-                                                 onSelect: function(d) { valueAccessor()(d); },
-                                                 onClear: function() { valueAccessor()(null); } 
-                                        });
+                                                     minDate: minDate,
+                                                     maxDate: maxDate,
+                                                     disableWeekends: disableWeekends,
+                                                     onSelect: function(d) { valueAccessor()(dateFormat(d, 'mm/dd/yyyy')); },
+                                                     onClose: function() { var el = element; 
+                                                                           // null out observable value if element is cleared
+                                                                           if ( el.value == '' ) {   
+                                                                               valueAccessor()(null);
+                                                                           }
+                                                                         }
+                                                  });
         },
         update: function(element, valueAccessor, allBindings) {
 
                var valueUnwrapped = ko.unwrap(valueAccessor());
- 
+
                valueAccessor().pikstore.setDate(valueUnwrapped);
+ 
            } 
        };
 
+ 
